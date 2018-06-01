a5dacdc1d47524f4d2d54562d4413a9df2f1f673
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2018-06-01 17:54:10 +0100

    MAINT: Move xorg config to Ansible

--- X/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -x
-mkdir -p $HOME/.config/i3/
-ln -s $PWD/i3-config $HOME/.config/i3/config
--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-# Requires my tweaked version of Stow
-for proj in amethyst bash emacs; do
-    stow --dotfiles --no-folding $proj
-done
