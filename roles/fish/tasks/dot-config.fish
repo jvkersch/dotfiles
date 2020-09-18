cafeaf19659121f9c10595820afa53509af8548b
Joris Vankerschaver <jvankerschaver@enthought.com>
2020-09-18 08:53:43 +0100

    MAINT: Update command

--- roles/fish/tasks/dot-config.fish
+++ roles/fish/tasks/dot-config.fish
@@ -31,8 +31,12 @@ alias P="edm run -e ls-platform -- "
 alias start_emacs="~/local/bin/emacs-26.3 &; disown"
 alias ec=emacsclient
 
-#
-alias start_dell="xrandr --output DP-4 --scale 2x2 --mode 2560x1440 --fb 5120x5040 --pos 0x0 --output eDP-1-1 --scale 1x1 --pos 640x2880"
+# Replace HDMI-0 by DP-4 for displayport
+alias start_dell="\
+    xrandr \
+    --output HDMI-0  --scale 2x2 --mode 2560x1440 --fb 5120x5040 --pos 0x0 \
+    --output eDP-1-1 --scale 1x1 --pos 640x2880"
+
 
 # Calibre books management
 function sync-books
