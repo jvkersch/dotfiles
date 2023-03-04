6b394232e882e176eaee8999a608f63ac054db21
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2023-03-04 21:26:43 +0900

    Years worth of changes to emacs config.

--- roles/emacs/tasks/dot-emacs.d/init.el
+++ roles/emacs/tasks/dot-emacs.d/init.el
@@ -1,13 +1,25 @@
+;(package-initialize)
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
+(setenv "PATH" (concat "/Library/TeX/texbin:"
+                       (getenv "PATH")))
+(add-to-list 'exec-path "/Library/TeX/texbin")
 
-(add-to-list 'load-path "/Users/jvkersch/local/org/emacs/site-lisp/org")
 (require 'org)
 (org-babel-load-file
- (file-truename
-  (expand-file-name "emacs.org"
-                    user-emacs-directory)))
+  (file-truename
+   (expand-file-name "emacs.org"
+                      user-emacs-directory)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
+ '(package-selected-packages
+   '(nov geiser lsp-julia ess lua-mode quarto-mode yaml-mode snakemake-mode ivy use-package undo-tree stan-mode rustic request python-docstring python-black org-roam-ui org-journal org-download org-bullets matlab-mode magit lsp-mode julia-repl julia-mode hungry-delete hcl-mode flycheck fish-mode exec-path-from-shell elpy dockerfile-mode deft cython-mode color-theme-sanityinc-tomorrow bm avy auctex ace-isearch)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
