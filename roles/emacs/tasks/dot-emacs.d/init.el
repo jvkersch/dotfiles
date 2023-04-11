f69f8e0bc563af942929669e9f8d5717fa340ca3
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2023-04-12 08:27:56 +0900

    Small tweaks

--- roles/emacs/tasks/dot-emacs.d/init.el
+++ roles/emacs/tasks/dot-emacs.d/init.el
@@ -16,7 +16,8 @@
  ;; If there is more than one, they won't work right.
  '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
  '(package-selected-packages
-   '(nov geiser lsp-julia ess lua-mode quarto-mode yaml-mode snakemake-mode ivy use-package undo-tree stan-mode rustic request python-docstring python-black org-roam-ui org-journal org-download org-bullets matlab-mode magit lsp-mode julia-repl julia-mode hungry-delete hcl-mode flycheck fish-mode exec-path-from-shell elpy dockerfile-mode deft cython-mode color-theme-sanityinc-tomorrow bm avy auctex ace-isearch)))
+   '(ace-window citar ox-json org-export-json nov geiser lsp-julia ess lua-mode quarto-mode yaml-mode snakemake-mode ivy use-package undo-tree stan-mode rustic request python-docstring python-black org-roam-ui org-journal org-download org-bullets matlab-mode magit lsp-mode julia-repl julia-mode hungry-delete hcl-mode flycheck fish-mode exec-path-from-shell elpy dockerfile-mode deft cython-mode color-theme-sanityinc-tomorrow bm avy auctex ace-isearch))
+ '(warning-suppress-types '(((python python-shell-completion-native-turn-on-maybe)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
